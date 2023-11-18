7437c8a73defe0604732f82ee4e74af7df23988a
Farid Aliev <hfe.as@yandex.ru>
2023-11-18 04:04:22 +0300

    feat: added new item view and its functionality

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from .models import Item
+from .forms import NewItemForm
 
 # Create your views here.
 
@@ -12,3 +14,23 @@ def detail(request, pk):
         'item': item,
         'related_items': related_items
     })
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'New Title'
+    })
+    
