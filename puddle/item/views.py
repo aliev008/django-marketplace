211892e6b9ffd07f2ef42b4f7fdac45ba4fe403c
Farid Aliev <hfe.as@yandex.ru>
2023-11-19 18:02:47 +0300

    feat: added simple search and category filters in the "browse" bookmark

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import redirect, render, get_object_or_404
+from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 
-from .models import Item
+from .models import Item, Category
 from .forms import NewItemForm, EditItemForm
 
 # Create your views here.
+
+def items(request):
+    items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+    category_id = request.GET.get('category', 0)
+    query = request.GET.get('query', '')
+    
+    if category_id:
+        items = Item.objects.filter(category_id=category_id)
+
+    if query:
+        items = Item.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id)
+    })
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -12,7 +32,7 @@ def detail(request, pk):
 
     return render(request, 'item/detail.html', {
         'item': item,
-        'related_items': related_items
+        'related_items': related_items,
     })
 
 @login_required
